70682a8fcefea35a363e24674fb3ba4f24f42ef2
Jake Collins <jake.james.collins@hotmail.com>
2018-11-07 10:19:31 +0200

    first merge

--- js/pages/nav-bar.cmp.js
+++ js/pages/nav-bar.cmp.js
@@ -4,7 +4,7 @@ export default {
     template: `
       <nav class="nav-bar">
       <router-link exact to="/">Home</router-link><span> | </span>
-      <router-link to="/">About</router-link><span> 
+      <router-link to="/">About</router-link>
   </nav>
   `
 }

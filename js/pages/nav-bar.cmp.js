4955c348c7508432fcbbadff2b3bc2adf66d4558
edoco89 <edo1cohen@gmail.com>
2018-11-07 10:50:26 +0200

    commit3

--- js/pages/nav-bar.cmp.js
+++ js/pages/nav-bar.cmp.js
@@ -4,11 +4,8 @@ export default {
     template: `
       <nav class="nav-bar">
       <router-link exact to="/">Home</router-link><span> | </span>
-<<<<<<< HEAD
-=======
       <router-link exact to="/emailApp">Email App</router-link><span> | </span>
       <router-link exact to="/keepApp">Keep App</router-link><span> | </span>
->>>>>>> 140522b07925bf58fdcc2dfa5ad0e9f7bb347cdc
       <router-link to="/">About</router-link>
   </nav>
   `
